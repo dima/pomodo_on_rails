f233cb1070a07f8a325f114084a959aeaa4b0321
Dima Berastau <dima@ruboss.com>
2008-12-25 23:17:14 -0800

    Got rid of :root options.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,9 +8,8 @@ class ProjectsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @projects }
-      format.fxml  { render :fxml => @projects.to_fxml(:root => Project.to_s.underscore.pluralize,
-        :include => {:sprints => {:root => Sprint.to_s.underscore.pluralize, 
-          :include => {:tasks => {:root => Task.to_s.underscore.pluralize, :methods => 
+      format.fxml  { render :fxml => @projects.to_fxml(:include => {:sprints => 
+        {:include => {:tasks => {:methods => 
           [:total_time, :total_time_today, :total_time_this_week, :total_time_this_month]}}}}) }    
     end
   end
