877181bc980c5e6a00630f60f88b0f298a74d3a1
Dima Berastau <dima.berastau@gmail.com>
2009-05-28 11:22:38 -0700

    Updated the session controller to move current_account setting higher up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
     logout_keeping_session!
     account = Account.authenticate(params[:login], params[:password])
     if account
+      self.current_account = account
       # Protects against session fixation attacks, causes request forgery
       # protection if user resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
@@ -18,7 +19,6 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
             
-      self.current_account = account
       respond_to do |format|
         format.html do
           redirect_back_or_default('/')
